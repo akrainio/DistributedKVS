8d20510e6857ec05183e8f82befe6beb9a911d5a
akrainio <andreikrainiouk@gmail.com>
2017-08-17 13:54:06 -0700

    Update ConsistentHashTest.scala

--- src/test/java/com/akrainio/cmps128/hw4/ConsistentHashTest.scala
+++ src/test/java/com/akrainio/cmps128/hw4/ConsistentHashTest.scala
@@ -1,39 +1,39 @@
-//package com.akrainio.cmps128.hw4
-//
-//import org.junit.Assert.assertEquals
-//import org.junit.{AfterClass, BeforeClass, Test}
-//
-////noinspection AccessorLikeMethodIsUnit
-//class ConsistentHashTest {
-//
-//  @Test
-//  def hashToNodeTest(): Unit = {
-//
-//  }
-//
-//  @Test
-//  def addNodeTest(): Unit = {
-//
-//  }
-//
-//  @Test
-//  def removeNodeTest(): Unit = {
-//
-//  }
-//
-//  @Test
-//  def getNodeTest(): Unit = {
-//
-//  }
-//
-//  @Test
-//  def getNodeEmptyTest(): Unit = {
-//
-//  }
-//
-//  @Test
-//  def rebalanceTest(): Unit = {
-//
-//  }
-//
-//}
+package com.akrainio.cmps128.hw4
+
+import org.junit.Assert.assertEquals
+import org.junit.{AfterClass, BeforeClass, Test}
+
+//noinspection AccessorLikeMethodIsUnit
+class ConsistentHashTest {
+
+ @Ignore @Test
+ def hashToNodeTest(): Unit = {
+
+ }
+
+ @Ignore @Test
+ def addNodeTest(): Unit = {
+
+ }
+
+ @Ignore @Test
+ def removeNodeTest(): Unit = {
+
+ }
+
+ @Ignore @Test
+ def getNodeTest(): Unit = {
+
+ }
+
+ @Ignore @Test
+ def getNodeEmptyTest(): Unit = {
+
+ }
+
+ @Ignore @Test
+ def rebalanceTest(): Unit = {
+
+ }
+
+}
